b6a74195742e4fabd10f608162c6a379db0e4788
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-04-15 18:18:15 +0100

    alguns livros e mais bonito

--- read_cycle/lib/components/post_tile.dart
+++ read_cycle/lib/components/post_tile.dart
@@ -14,57 +14,69 @@ class PostTile extends StatelessWidget {
       ),
       margin: EdgeInsets.only(left: 20),
       padding: EdgeInsets.all(25),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.asset(
-            post.bookImage,
-            height: 100,
-          ),
-          Text(
-            post.bookTitle,
-            style: TextStyle(
-              fontSize: 15,
+      child: SizedBox(
+        width: 110,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              post.bookImage,
+              height: 100,
             ),
-          ),
-          Text(
-            post.bookAuthor,
-            style: TextStyle(
-              fontSize: 12
+            Text(
+              post.bookTitle,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                fontSize: 15,
+              ),
             ),
-          ),
-          SizedBox(
-            width: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  post.userName,
-                  style: TextStyle(
-                    color: const Color.fromARGB(255, 114, 114, 114),
-                    fontSize: 14,
-                  ),
-                ),
-                Row(
-                 children: [
-                    Icon(
-                      Icons.star,
-                      color: Colors.amber,
-                      size: 20,
-                    ),
-                    Text(
-                      post.userRating,
+            Text(
+              post.bookAuthor,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                fontSize: 12
+              ),
+            ),
+            SizedBox(
+              width: 110,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: 60,
+                    child: Text(
+                      post.userName,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
                       style: TextStyle(
                         color: const Color.fromARGB(255, 114, 114, 114),
-                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
                       ),
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  Row(
+                   children: [
+                      Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                        size: 20,
+                      ),
+                      Text(
+                        post.userRating,
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 114, 114, 114),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
