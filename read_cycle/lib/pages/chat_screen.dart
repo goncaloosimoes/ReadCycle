b965a2e4bc8e35d45fec01e342390c0393d92150
Gonçalo Simões <151958998+goncaloosimoes@users.noreply.github.com>
2025-04-15 11:58:43 +0100

    chat updates

--- read_cycle/lib/pages/chat_screen.dart
+++ read_cycle/lib/pages/chat_screen.dart
@@ -5,23 +5,58 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> chats = ['Chat 1', 'Chat 2', 'Chat 3'];
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Olá outra vez!'),
+        title: Text('Chats Disponíveis'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              // Ação ao clicar no ícone de pesquisa
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {
+              // Ação ao clicar no ícone de mais opções
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Chat Screen',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold
-              ),  
-            )
-          ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: ListView.separated(
+          itemCount: chats.length,
+          separatorBuilder: (context, index) => Divider(),
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/blank_profile.jpg'),
+                  radius: 25,
+                ),
+                title: Text(
+                  chats[index],
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18, // Aumentar o tamanho da fonte
+                  ),
+                ),
+                subtitle: Text('Última mensagem...'),
+                trailing: Icon(
+                  Icons.message,
+                  color: Theme.of(context).primaryColor,
+                ),
+                onTap: () {
+                  // Ação ao clicar no chat
+                },
+              ),
+            );
+          },
         ),
       ),
     );
