dcd9b9b4b21c127050bd257123d6943f2ce2ffc1
Apmds <ap1mrfj@gmail.com>
2025-04-26 11:45:53 +0100

    barra de pesquisa funciona melhor

--- read_cycle/lib/pages/home_screen.dart
+++ read_cycle/lib/pages/home_screen.dart
@@ -15,13 +15,36 @@ class HomeScreen extends StatefulWidget {
 
 class _MainState extends State<HomeScreen> {
   bool searchBarVisible = false;
+  final TextEditingController _controller = TextEditingController();
 
-  void toggleSearchBarVisibility() {
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void searchButtonFunctionality() {
+    if (!searchBarVisible) {
+      setState(() {
+        searchBarVisible = !searchBarVisible;
+        if (searchBarVisible) {
+          print("show!");
+        }
+      });
+    } else {
+      // Passar para a página de pesquisa
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const ProfileScreen(),
+        ),
+      );
+    }
+  }
+
+  void hideSearchBar() {
     setState(() {
-      searchBarVisible = !searchBarVisible;
-      if (searchBarVisible) {
-        print("show!");
-      }
+      searchBarVisible = false;
     });
   }
 
@@ -44,92 +67,105 @@ class _MainState extends State<HomeScreen> {
       BooksTile(title: 'Comédia', postTiles: postTiles),
     ];
 
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(65),
-        child: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          elevation: 0,
-          flexibleSpace: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 200, // enough width for interaction
-                    height: 60,
-                    child: Stack(
-                      alignment: AlignmentDirectional.centerStart,
-                      clipBehavior: Clip.none,
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (didPop, result) {
+        if (didPop) {
+          // It actually popped
+        } else {
+          // It was canceled or not popped
+          print('Screen was popped with result: $result');
+          hideSearchBar();
+        }
+      },
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(65),
+          child: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            elevation: 0,
+            flexibleSpace: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 200, // enough width for interaction
+                      height: 60,
+                      child: Stack(
+                        alignment: AlignmentDirectional.centerStart,
+                        clipBehavior: Clip.none,
+                        children: [
+                          Text(
+                            'ReadCycle',
+                            style: TextStyle(color: Colors.black, fontSize: 25),
+                          ),
+                          Positioned(
+                            left: 20,
+                            width: 200,
+                            child: Visibility(
+                              visible: searchBarVisible,
+                              maintainSize: true,
+                              maintainState: true,
+                              maintainAnimation: true,
+                              child: TextField(
+                                controller: _controller,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                                  fillColor: Color.fromARGB(255, 241, 241, 241),
+                                  filled: true,
+                                  hintText: 'Enter a search term',
+                                ),
+                              ),
+                            ),
+                          ),
+
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Text(
-                          'ReadCycle',
-                          style: TextStyle(color: Colors.black, fontSize: 25),
+                        IconButton(
+                          onPressed: searchButtonFunctionality,
+                          icon: const Icon(Icons.search),
                         ),
-                        Positioned(
-                          left: 20,
-                          width: 200,
-                          child: Visibility(
-                            visible: searchBarVisible,
-                            maintainSize: true,
-                            maintainState: true,
-                            maintainAnimation: true,
-                            child: TextField(
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
-                                fillColor: Color.fromARGB(255, 241, 241, 241),
-                                filled: true,
-                                hintText: 'Enter a search term',
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const ProfileScreen(),
+                              ),
+                            );
+                          },
+                          child: CircleAvatar(
+                            radius: 30,
+                            backgroundColor: Colors.white,
+                            child: CircleAvatar(
+                              radius: 28,
+                              backgroundImage: AssetImage(
+                                currentUser.profileImagePath,
                               ),
                             ),
                           ),
                         ),
-
                       ],
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        onPressed: toggleSearchBarVisibility,
-                        icon: const Icon(Icons.search),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const ProfileScreen(),
-                            ),
-                          );
-                        },
-                        child: CircleAvatar(
-                          radius: 30,
-                          backgroundColor: Colors.white,
-                          child: CircleAvatar(
-                            radius: 28,
-                            backgroundImage: AssetImage(
-                              currentUser.profileImagePath,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
         ),
-      ),
-      body: ListView.builder(
-        scrollDirection: Axis.vertical,
-        itemCount: bookTiles.length,
-        itemBuilder: (context, index) => bookTiles[index],
+        body: ListView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: bookTiles.length,
+          itemBuilder: (context, index) => bookTiles[index],
+        ),
       ),
     );
   }
