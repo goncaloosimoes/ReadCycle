08e0a40c01180f4fc0b2e7b332e4d333b39cd096
Apmds <ap1mrfj@gmail.com>
2025-04-26 22:37:58 +0100

    search results

---
+++ read_cycle/lib/components/search_result.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:read_cycle/classes/post.dart';
+import 'package:read_cycle/pages/book_details_screen.dart';
+
+class SearchResult extends StatelessWidget {
+  final Post post;
+  const SearchResult({super.key, required this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // Mudar para a página do post associado
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => BookDetailsScreen(post: post)),
+        );
+      },
+      child: SizedBox(
+        height: 150,
+        child: ColoredBox(
+          color: const Color.fromARGB(255, 246, 237, 218),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: Image.asset(
+                      post.book.coverImagePath,
+                      height: 120,
+                    )
+                  )
+                ),
+                Expanded(
+                  flex: 4,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+
+                    children: [
+                      Text(
+                        post.book.title,
+                        softWrap: false,
+                        overflow: TextOverflow.fade,
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        post.book.author,
+                        softWrap: false,
+                        overflow: TextOverflow.fade,
+                      ),
+                      Text(
+                        "${post.book.pages} páginas",
+                        softWrap: false,
+                        overflow: TextOverflow.fade,
+                      ),
+                      Text(
+                        "${post.user.name} ${post.user.rating} ⭐",
+                        softWrap: false,
+                        overflow: TextOverflow.fade,
+                        style: TextStyle(
+                            fontSize: 12.5,
+                        ),
+                      ),
+                    ],
+                  )
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("📍 ${post.location}"),
+                      Text("📅 há ${5} dias"),
+                    ],
+                  )
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- read_cycle/lib/pages/search_screen.dart
+++ read_cycle/lib/pages/search_screen.dart
@@ -1,5 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:read_cycle/classes/post.dart';
+import 'package:read_cycle/components/search_result.dart';
+import 'package:read_cycle/data/posts.dart';
 
 class SearchScreen extends StatefulWidget {
   final String searchText;
@@ -10,14 +13,42 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _MainState extends State<SearchScreen> {
+  String searchText = "";
   final TextEditingController _controller = TextEditingController();
+  List<SearchResult> searchResults = [];
 
   _MainState();
+
+  void reloadPage() {
+    setState(() {
+      searchText = _controller.text;
+      getRelevantPosts();
+    });
+  }
+
+  void getRelevantPosts() {
+    searchResults = [];
+    
+    // Listar posts relevantes
+    for (Post post in appPosts) {
+      if (searchText.toLowerCase().contains(post.book.title.toLowerCase())) {
+        searchResults.add(SearchResult(post: post));
+      }
+      else if (post.book.title.toLowerCase().contains(searchText.toLowerCase())) {
+        searchResults.add(SearchResult(post: post));
+      }
+    }
+  }
 
   @override
   void initState() {
     super.initState();
+
+    // Definir valores iniciais
     _controller.text = widget.searchText;
+    searchText = widget.searchText;
+
+    getRelevantPosts();
   }
 
   @override
@@ -32,13 +63,16 @@ class _MainState extends State<SearchScreen> {
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 45),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     SizedBox(
                       width: 180,
                       child: TextField(
                         controller: _controller,
+                        onSubmitted: (value) {
+                          reloadPage();
+                        },
                         decoration: InputDecoration(
                           border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
                           fillColor: Color.fromARGB(255, 241, 241, 241),
@@ -46,7 +80,11 @@ class _MainState extends State<SearchScreen> {
                           hintText: 'Pesquisa...',
                         ),
                       )
-                    )
+                    ),
+                    IconButton(
+                      onPressed: reloadPage,
+                      icon: const Icon(Icons.search)
+                    ),
                   ],
                 ),
             )
@@ -54,15 +92,27 @@ class _MainState extends State<SearchScreen> {
         )
       ),
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Column(
-          children: [
-            Text(
-              "Resultados para \"${widget.searchText}\"",
-              style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          
+          itemCount: searchResults.length + 1,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return Padding(
+                padding: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                child: Text(
+                "Resultados para \"$searchText\"",
+                style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+              ));
+            }
+            return Padding(
+              padding: EdgeInsets.only(bottom: 5),
+              child: searchResults[index - 1]
+            );
+          },
+        )
       ),
     );
   }
