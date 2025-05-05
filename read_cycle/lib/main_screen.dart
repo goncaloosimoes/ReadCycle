f0e9c38fed417ad0dfaf917bf3830cdd671c7b1a
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-05-05 23:11:58 +0100

    botão já não sobe com o teclado

--- read_cycle/lib/main_screen.dart
+++ read_cycle/lib/main_screen.dart
@@ -29,46 +29,51 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-        body: screenWidgets[_selectedIdx],
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: SizedBox(
-          height: 70,
-          width: 70,
-          child: FloatingActionButton(
-            onPressed: () => _changeIdx(1),
-            backgroundColor: const Color.fromARGB(255, 199, 156, 28),
-            shape: CircleBorder(),
-            child: Icon(Icons.add, size: 40,),
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          shape: CircularNotchedRectangle(),
-          notchMargin: 7,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              IconButton(
-                iconSize: 35,
-                icon: Icon(
-                  _selectedIdx == 0 ? Icons.home_filled : Icons.home_outlined
-                ),
-                onPressed: () => _changeIdx(0),
+    final bool isKeyboardOpen = MediaQuery.of(context).viewInsets.bottom > 0;
+
+    return Scaffold(
+      body: screenWidgets[_selectedIdx],
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: isKeyboardOpen
+          ? null
+          : SizedBox(
+              height: 70,
+              width: 70,
+              child: FloatingActionButton(
+                onPressed: () => _changeIdx(1),
+                backgroundColor: const Color.fromARGB(255, 199, 156, 28),
+                shape: CircleBorder(),
+                child: Icon(Icons.add, size: 40),
               ),
-              SizedBox(width: 20,),
-              IconButton(
-                iconSize: 35,
-                icon: Badge(
-                  label: Text(_numNotifications.toString()),
-                  child: Icon(
-                    _selectedIdx == 2 ? Icons.chat : Icons.chat_outlined,
-                  ),
-                ),
-                onPressed: () => _changeIdx(2),
+            ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 7,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              iconSize: 35,
+              icon: Icon(
+                _selectedIdx == 0 ? Icons.home_filled : Icons.home_outlined,
               ),
-            ]
-          ),
+              onPressed: () => _changeIdx(0),
+            ),
+            SizedBox(width: 20),
+            IconButton(
+              iconSize: 35,
+              icon: Badge(
+                label: Text(_numNotifications.toString()),
+                child: Icon(
+                  _selectedIdx == 2 ? Icons.chat : Icons.chat_outlined,
+                ),
+              ),
+              onPressed: () => _changeIdx(2),
+            ),
+          ],
         ),
-      );
-    }
+      ),
+    );
+  }
+
 }
