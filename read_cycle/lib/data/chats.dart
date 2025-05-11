48dfaa525ed05e431e40355102deeadf0a2fee90
Apmds <ap1mrfj@gmail.com>
2025-05-11 16:35:04 +0100

    Emma dá O Hobbit por default

--- read_cycle/lib/data/chats.dart
+++ read_cycle/lib/data/chats.dart
@@ -6,5 +6,16 @@ import 'package:read_cycle/data/users.dart';
 
 var _random = Random();
 List<Chat> chats = [
-  for (var user in appUsers) Chat(user: user, myBooks: [], otherBooks: [for (Book book in user.books) if (_random.nextBool()) book], messages: [], read: false),
+  for (var user in appUsers) Chat(
+    user: user,
+    myBooks: [],
+    otherBooks: [
+      if (user.name == "Emma")  // Emma apenas disponibiliza o hobbit
+        user.books.firstWhere((book) => book.title.toLowerCase().contains("hobbit"))
+      else
+        for (Book book in user.books) if (_random.nextBool()) book
+      ],
+      messages: [],
+      read: false
+    ),
 ];
