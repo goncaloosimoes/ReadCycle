f15503d14cebf8714d9cb2d921707218dcb83ad2
Apmds <ap1mrfj@gmail.com>
2025-05-20 11:59:23 +0100

    timestamp in messages

--- read_cycle/lib/components/message_widget.dart
+++ read_cycle/lib/components/message_widget.dart
@@ -10,18 +10,32 @@ class MessageWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Align(
       alignment: (message.fromUser) ? Alignment.centerRight : Alignment.centerLeft,
-      child: Container(
-        padding: EdgeInsets.all(12),
-        margin: EdgeInsets.symmetric(vertical: 4),
-        decoration: BoxDecoration(
-          color: (message.fromUser) ? Theme.of(context).primaryColorLight : Colors.grey[300],
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Text(
-          message.text,
-          style: TextStyle(fontSize: 16, color: const Color.fromARGB(255, 0, 0, 0)),
-        ),
-      ),
+      child: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.all(15),
+            margin: EdgeInsets.symmetric(vertical: 4),
+            decoration: BoxDecoration(
+              color: (message.fromUser) ? Theme.of(context).primaryColorLight : Colors.grey[300],
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Text(
+                  message.text,
+                  style: TextStyle(fontSize: 16, color: const Color.fromARGB(255, 0, 0, 0)),
+                ),
+          ),
+          Positioned(
+            bottom: 5,
+            right: 5,
+            child: Text(
+              message.formattedTime,
+              style: TextStyle(
+                fontSize: 11,
+              )
+            )
+          )
+        ],
+      )
     );
   }
 }
