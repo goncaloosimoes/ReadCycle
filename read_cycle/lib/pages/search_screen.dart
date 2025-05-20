eff082d6400d156ccfa5b8ca051945dbb4354293
Apmds <ap1mrfj@gmail.com>
2025-05-20 12:39:09 +0100

    fix search bar overflow fix

--- read_cycle/lib/pages/search_screen.dart
+++ read_cycle/lib/pages/search_screen.dart
@@ -90,36 +90,8 @@ class _MainState extends State<SearchScreen> {
         child: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           elevation: 0,
-          flexibleSpace: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 45, vertical: 5),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 170,
-                    height: 50,
-                    child: TextField(
-                      controller: _controller,
-                      onSubmitted: (value) {
-                        reloadPage();
-                      },
-                      onChanged: (value) {
-                        reloadPage();
-                      },
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(30)),
-                          borderSide: BorderSide.none,
-                        ),
-                        fillColor: Color.fromARGB(255, 241, 241, 241),
-                        filled: true,
-                        hintText: 'Pesquisa um livro...',
-                      ),
-                    ),
-                  ),
-                  IconButton(
+          actions: [
+            IconButton(
                     onPressed: reloadPage,
                     icon: const Icon(Icons.search),
                   ),
@@ -165,6 +137,37 @@ class _MainState extends State<SearchScreen> {
                           ),
                         ],
                   ),
+          ],
+          flexibleSpace: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 45, vertical: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 220,
+                    height: 50,
+                    child: TextField(
+                      controller: _controller,
+                      onSubmitted: (value) {
+                        reloadPage();
+                      },
+                      onChanged: (value) {
+                        reloadPage();
+                      },
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                          borderSide: BorderSide.none,
+                        ),
+                        fillColor: Color.fromARGB(255, 241, 241, 241),
+                        filled: true,
+                        hintText: 'Pesquisa um livro...',
+                      ),
+                    ),
+                  ),
+                  
                 ],
               ),
             ),
